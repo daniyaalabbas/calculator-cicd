6fc089d0e9c0f01c2de2496f20b1c9212bf2e2cf
Mohammed Abbas Ali <abbasdaniyaal@gmail.com>
2026-08-02 15:57:58 +0530

    Fix health endpoint

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 APP_TITLE = os.getenv("APP_TITLE", "Calculator App")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "Development")
 
-@app.route("/health")
-def health():
-    return "FAILED", 500
-
-
 
 
 @app.route("/")
